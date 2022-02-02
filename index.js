3f192cd5fd4ce532c7c2681ab4d0b7eb09d4183d
Elizabeth Klenczar <eklenczar@gmail.com>
2022-02-01 23:05:12 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    const headQ = 42;
+    if (blocks <= headQ) {
+        return headQ - blocks
+    } else (blocks >= headQ) 
+        return blocks - headQ
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    distanceTravelledInFeet = Math.abs(start - destination) * 264  
+    return distanceTravelledInFeet
+}    
+
+function calculatesFarePrice(start, destination) {
+    calculatesFarePrice = Math.abs(start - destination) * 264
+    let distance = calculatesFarePrice
+        if (distance <= 400) 
+            return 0
+        else if (distance >= 401 && distance <= 2000) 
+            return (distance - 400) * .02
+        else if (distance >= 2000 && distance <= 2499)
+            return 25
+        else if (distance > 2500)
+            return 'cannot travel that far'           
+    }
